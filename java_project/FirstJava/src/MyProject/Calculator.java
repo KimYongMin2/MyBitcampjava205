441d02b0c9accac6d4ecd80a620312313806b52d
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-05-24 16:09:01 +0900

    반환형 및 출력 바꾸기

--- java_project/FirstJava/src/MyProject/Calculator.java
+++ java_project/FirstJava/src/MyProject/Calculator.java
@@ -1,19 +1,19 @@
 package MyProject;
 
 public class Calculator {
-    public int add(int num1, int num2){
-        return num1 + num2;
+    public void add(int num1, int num2){
+        System.out.println(num1 + num2);
     }
 
-    public int sub(int num1, int num2){
-        return num1 - num2;
+    public void sub(int num1, int num2){
+        System.out.println(num1 - num2);
     }
 
-    public int mul(int num1, int num2){
-        return num1 * num2;
+    public void mul(int num1, int num2){
+        System.out.println(num1 * num2);
     }
 
-    public int iMul(int num1, int num2){
-        return num1 / num2;
+    public void iMul(int num1, int num2){
+        System.out.println(num1 / num2);
     }
 }
