a8295f92c1c9d5be37ef7764823fc9d0c52b80cb
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-05-24 18:18:10 +0900

    scanner사용 및 원넓이,둘레 추가

--- java_project/FirstJava/src/MyProject/Calculator.java
+++ java_project/FirstJava/src/MyProject/Calculator.java
@@ -1,5 +1,7 @@
 package MyProject;
 
+import java.util.Scanner;
+
 public class Calculator {
     public void plus(int num1, int num2){
         System.out.println(num1 + num2);
@@ -13,9 +15,18 @@ public class Calculator {
         System.out.println(num1 * num2);
     }
 
-    public void div(int num1, int num2){
+    public void div(double num1, double num2){
         System.out.println(num1 / num2);
     }
+
+    public void round(double radius){
+        System.out.println(2 * Math.PI * radius);
+    }
+
+    public void area(double radius){
+        System.out.println(Math.PI * radius * radius);
+    }
+
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
@@ -23,5 +34,16 @@ public class Calculator {
         calculator.minus(20, 10);
         calculator.div(10, 3);
         calculator.multi(2, 5);
+        System.out.print("반지름을 입력해주세요 : ");
+        double radius = getInputInteger();
+        calculator.round(radius);
+        calculator.area(radius);
+
+    }
+
+    private static int getInputInteger() {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        return  Integer.parseInt(input);
     }
 }
