07a677491b192bf236a55ba030f2ed3d2c5ce48a
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-05-24 16:10:52 +0900

    함수이름 변환

--- java_project/FirstJava/src/MyProject/Calculator.java
+++ java_project/FirstJava/src/MyProject/Calculator.java
@@ -1,19 +1,19 @@
 package MyProject;
 
 public class Calculator {
-    public void add(int num1, int num2){
+    public void plus(int num1, int num2){
         System.out.println(num1 + num2);
     }
 
-    public void sub(int num1, int num2){
+    public void minus(int num1, int num2){
         System.out.println(num1 - num2);
     }
 
-    public void mul(int num1, int num2){
+    public void multi(int num1, int num2){
         System.out.println(num1 * num2);
     }
 
-    public void iMul(int num1, int num2){
+    public void div(int num1, int num2){
         System.out.println(num1 / num2);
     }
 }
