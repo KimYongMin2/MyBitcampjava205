fa9511f329f7ea50c86c9a3dd48856e251446a6a
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-05-24 17:18:58 +0900

    Main문 추가

--- java_project/FirstJava/src/MyProject/Member.java
+++ java_project/FirstJava/src/MyProject/Member.java
@@ -2,6 +2,12 @@ package MyProject;
 
 public class Member {
     public static void main(String[] args) {
-        System.out.println("김용민");
+        String name = "김용민";
+        int age = 26;
+        double height = 174;
+        boolean hasBook = false;
+        System.out.println("제 이름은 김용민입니다.");
+        System.out.println("이름 : " + name + "나이 : " + age + "몸무게 : " + height);
+        System.out.println("책 보유 여부 : " + hasBook);
     }
 }
