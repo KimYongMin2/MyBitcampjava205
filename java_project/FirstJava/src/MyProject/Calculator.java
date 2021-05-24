f667a04a2e375b2b59047983dd14f6c65adbeffa
DESKTOP-KIPKKJA\kym96 <kym960304@naver.com>
2021-05-24 16:53:35 +0900

    Calculator main문 추가

--- java_project/FirstJava/src/MyProject/Calculator.java
+++ java_project/FirstJava/src/MyProject/Calculator.java
@@ -16,4 +16,12 @@ public class Calculator {
     public void div(int num1, int num2){
         System.out.println(num1 / num2);
     }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.plus(6,3);
+        calculator.minus(6,3);
+        calculator.multi(6,3);
+        calculator.div(6,3);
+    }
 }
