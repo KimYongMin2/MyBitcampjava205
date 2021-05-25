022c2528156582080884c78733533c8f9cb62f15
KimYongMin2 <kym960304@naver.com>
2021-05-25 10:05:10 +0900

    현재날짜 변환

--- java_project/FirstJava/src/myProjectExtends/PersonMain.java
+++ java_project/FirstJava/src/myProjectExtends/PersonMain.java
@@ -2,6 +2,7 @@ package myProjectExtends;
 
 public class PersonMain {
     public static void main(String[] args) {
+        String idNumber = "960304";
         Person person = new Person("사람" , "960304");
         Male male = new Male("김용민", "960304-1******");
         Female female = new Female("김용순", "9504012******");
