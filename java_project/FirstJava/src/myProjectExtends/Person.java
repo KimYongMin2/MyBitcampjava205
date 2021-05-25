599a5aa46cc8c037b13b0c4aeeb02e5d0c164062
KimYongMin2 <kym960304@naver.com>
2021-05-25 10:04:31 +0900

    현재날짜 변환

--- java_project/FirstJava/src/myProjectExtends/Person.java
+++ java_project/FirstJava/src/myProjectExtends/Person.java
@@ -1,5 +1,8 @@
 package myProjectExtends;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Person {
     String name;
     String idNumber;
@@ -9,7 +12,7 @@ public class Person {
         this.idNumber = idNumber;
     }
     public void hello(){
-        age = 121 - Integer.parseInt(idNumber.substring(0,1));
+        age = Integer.parseInt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy"))) - Integer.parseInt(idNumber.substring(0,2))-1899;
         System.out.println("안녕하세요. 저는 " + name + "입니다. " + age + "살 입니다.");
     }
 }
