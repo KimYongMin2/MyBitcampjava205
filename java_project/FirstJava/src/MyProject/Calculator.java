6ab89628fef0b191a34e2e459fe8005a5c5055a9
KimYongMin2 <kym960304@naver.com>
2021-05-25 11:37:42 +0900

    계산기 변경

--- java_project/FirstJava/src/MyProject/Calculator.java
+++ java_project/FirstJava/src/MyProject/Calculator.java
@@ -3,41 +3,41 @@ package MyProject;
 import java.util.Scanner;
 
 public class Calculator {
-    public void plus(int num1, int num2){
-        System.out.println(num1 + num2);
+    public int plus(int num1, int num2){
+        return num1 + num2;
     }
 
-    public void minus(int num1, int num2){
-        System.out.println(num1 - num2);
+    public int minus(int num1, int num2){
+        return num1 - num2;
     }
 
-    public void multi(int num1, int num2){
-        System.out.println(num1 * num2);
+    public int multi(int num1, int num2){
+        return num1 * num2;
     }
 
-    public void div(double num1, double num2){
-        System.out.println(num1 / num2);
+    public double div(double num1, double num2){
+        return num1 / num2;
     }
 
-    public void round(double radius){
-        System.out.println(2 * Math.PI * radius);
+    public double round(double radius){
+        return 2 * Math.PI * radius;
     }
 
-    public void area(double radius){
-        System.out.println(Math.PI * radius * radius);
+    public double area(double radius){
+        return Math.PI * radius * radius;
     }
 
 
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
-        calculator.plus(10, 20);
-        calculator.minus(20, 10);
-        calculator.div(10, 3);
-        calculator.multi(2, 5);
+        System.out.println(calculator.plus(10, 20));
+        System.out.println(calculator.minus(20, 10));
+        System.out.println(calculator.div(10, 3));
+        System.out.println(calculator.multi(2, 5));
         System.out.print("반지름을 입력해주세요 : ");
         double radius = getInputInteger();
-        calculator.round(radius);
-        calculator.area(radius);
+        System.out.println(calculator.round(radius));
+        System.out.println(calculator.area(radius));
 
     }
 
