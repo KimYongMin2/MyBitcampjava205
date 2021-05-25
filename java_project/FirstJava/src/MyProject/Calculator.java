b49768d8a95e8ea6248c0918ddfe6e949ed55f1c
KimYongMin2 <kym960304@naver.com>
2021-05-25 11:59:28 +0900

    묶기

--- java_project/FirstJava/src/MyProject/Calculator.java
+++ java_project/FirstJava/src/MyProject/Calculator.java
@@ -27,22 +27,33 @@ public class Calculator {
         return Math.PI * radius * radius;
     }
 
+    public void calculation(int num1, int num2){
+        System.out.println("더하기 : "  + this.plus(num1, num2));
+        System.out.println("빼기 : "  +this.minus(num1, num2));
+        System.out.println("곱하기 : "  +this.div(num1, num2));
+        System.out.println("나누기 : "  +this.multi(num1, num2));
+    }
+
+    public void calculationCircle(double radius){
+        System.out.println("원둘레 : "  +this.round(radius));
+        System.out.println("원넓이 : "  +this.area(radius));
+    }
+
 
     public static void main(String[] args) {
         System.out.print("num1 값을 입력하여 주세요 : ");
         int num1 = getInputInteger();
         System.out.print("num2 값을 입력하여 주세요 : ");
         int num2 = getInputInteger();
+
         Calculator calculator = new Calculator();
-        System.out.println("더하기 : "  + calculator.plus(num1, num2));
-        System.out.println("빼기 : "  +calculator.minus(num1, num2));
-        System.out.println("곱하기 : "  +calculator.div(num1, num2));
-        System.out.println("나누기 : "  +calculator.multi(num1, num2));
+
+        calculator.calculation(num1,num2);
+
         System.out.print("반지름을 입력해주세요 : ");
         double radius = getInputInteger();
-        System.out.println("원둘레 : "  +calculator.round(radius));
-        System.out.println("원넓이 : "  +calculator.area(radius));
 
+        calculator.calculationCircle(radius);
     }
 
     private static int getInputInteger() {
@@ -51,3 +62,4 @@ public class Calculator {
         return  Integer.parseInt(input);
     }
 }
+6
