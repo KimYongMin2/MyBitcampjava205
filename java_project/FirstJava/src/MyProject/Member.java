1f6aa930d55e65390902590e58d5a413fd26d9aa
KimYongMin2 <kym960304@naver.com>
2021-05-25 10:05:01 +0900

    현재날짜 변환

--- java_project/FirstJava/src/MyProject/Member.java
+++ java_project/FirstJava/src/MyProject/Member.java
@@ -1,11 +1,13 @@
 package MyProject;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
 public class Member {
     //04번
     void vaccinPossible(int birthYear){
-        int age = 2021-birthYear+1;
+        int age = Integer.parseInt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy")))-birthYear+1;
         if(age < 15 || age >= 65){
             System.out.println("무료예방접종이 가능합니다.");
         }else{
@@ -14,10 +16,9 @@ public class Member {
     }
 
     void healthScreenings(int birthYear){
-        int thisYear = 2021;
-        int age = 2021-birthYear+1;
+        int age = Integer.parseInt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy")))-birthYear+1;
         if(age >= 20){
-            if(age % 2 == thisYear % 2){
+            if(age % 2 == Integer.parseInt(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy"))) % 2){
                 System.out.println("건강검진 검사대상입니다.");
                 if(age >= 40){
                     System.out.println("암 검사도 가능합니다.");
@@ -35,7 +36,7 @@ public class Member {
         double height = 174;
         boolean hasBook = false;
         System.out.println("제 이름은 김용민입니다.");
-        System.out.println("이름 : " + name + "나이 : " + age + "몸무게 : " + height);
+        System.out.println("이름 : " + name + " 나이 : " + age + " 키 : " + height);
         System.out.println("책 보유 여부 : " + hasBook);
         // 04번
         Member member = new Member();
