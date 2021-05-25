aa918dbdb3a1b176e2f4a1f3920dc2ac02ef7f44
KimYongMin2 <kym960304@naver.com>
2021-05-25 11:16:09 +0900

    Person 파일 추가

--- java_project/FirstJava/src/MyProject/Person.java
+++ java_project/FirstJava/src/MyProject/Person.java
@@ -1,7 +1,26 @@
 package MyProject;
 
 public class Person {
-    public String memberName;
-    private String telNumber;
-    private String idNumber;
+    String name;
+    String telNum = "000-0000-0000";
+    String perNum1 = "700000-0000000";
+    long perNum2 = 7000000000000L;
+    void showData() {
+        System.out.println("회원 이름 : " + this.name);
+        System.out.println("회원의 주민번호1 : " + perNum1 );
+        System.out.println("회원의 주민번호2 : " + perNum2 );
+    }
+
+    public static void main(String[] args) {
+        Person person = new Person();
+
+        System.out.println("이름 : " + person.name );
+        person.name = "손흥민";
+        person.showData();
+
+    }
+
+
+
+
 }
