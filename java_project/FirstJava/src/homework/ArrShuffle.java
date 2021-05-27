c8ccc3266ab640f6d58627cea2cb9773f3e311b0
KimYongMin2 <kym960304@naver.com>
2021-05-27 15:43:52 +0900

    03번 문제
    메소드 빼기

--- java_project/FirstJava/src/homework/ArrShuffle.java
+++ java_project/FirstJava/src/homework/ArrShuffle.java
@@ -3,6 +3,25 @@ package homework;
 import java.util.Scanner;
 
 public class ArrShuffle {
+
+    private static int[][] setProcess() {
+        System.out.print("행(가로)수를 입력해주세요 : ");
+        int processionRow = getInputInt();
+        System.out.print("열(세로)수를 입력해주세요 : ");
+        int processionColumn = getInputInt();
+        int[][] score = new int[processionRow][processionColumn];
+        return score;
+    }
+
+    private static void setProcessScore(int[][] score) {
+        for (int i = 0; i < score.length; i++) {
+            for (int j = 0; j < score[i].length; j++) {
+                System.out.print((i + 1) + "번째의 " + (j + 1) + "번 수를 입력하여 주세요 : ");
+                score[i][j] = getInputInt();
+            }
+        }
+    }
+
     private static void ArrShuffle(int[][] arr){
         int[] temp;
         for (int i = arr.length-1; i > 0; i--) {
@@ -29,24 +48,13 @@ public class ArrShuffle {
     }
 
     public static void main(String[] args) {
-        System.out.print("행(가로)수를 입력해주세요 : ");
-        int processionRow = getInputInt();
-        System.out.print("열(세로)수를 입력해주세요 : ");
-        int processionColumn = getInputInt();
-        int[][] score = new int[processionRow][processionColumn];
-
-        for (int i = 0; i < score.length; i++) {
-            for (int j = 0; j < score[i].length; j++) {
-                System.out.print((i + 1) + "번째의 " + (j + 1) + "번 수를 입력하여 주세요 : ");
-                score[i][j] = getInputInt();
-            }
-        }
-
+        int[][] score = setProcess();
+        setProcessScore(score);
         showInputScore(score);
-
         System.out.println("순서를 바꿉니다.");
         ArrShuffle(score);
-
         showInputScore(score);
     }
+
+
 }
