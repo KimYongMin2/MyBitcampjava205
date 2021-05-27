d48c484c95d98626554e9ccbbfa751a06071a073
KimYongMin2 <kym960304@naver.com>
2021-05-27 15:15:39 +0900

    03번문제
    쓸모없는 변수선언 삭제

--- java_project/FirstJava/src/homework/ArrShuffle.java
+++ java_project/FirstJava/src/homework/ArrShuffle.java
@@ -29,12 +29,10 @@ public class ArrShuffle {
     }
 
     public static void main(String[] args) {
-        int processionRow;
-        int processionColumn;
         System.out.print("행(가로)수를 입력해주세요 : ");
-        processionRow = getInputInt();
+        int processionRow = getInputInt();
         System.out.print("열(세로)수를 입력해주세요 : ");
-        processionColumn = getInputInt();
+        int processionColumn = getInputInt();
         int[][] score = new int[processionRow][processionColumn];
 
         for (int i = 0; i < score.length; i++) {
