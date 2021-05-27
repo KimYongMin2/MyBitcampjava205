1f64492aea154d78d4f862eea6c232a0b02cb9da
KimYongMin2 <kym960304@naver.com>
2021-05-27 18:40:49 +0900

    정보 메인
    함수빼기

--- java_project/FirstJava/src/ex/arrayTest/ArrayMain.java
+++ java_project/FirstJava/src/ex/arrayTest/ArrayMain.java
@@ -16,27 +16,38 @@ public class ArrayMain {
 
         int button=0;
         while (!(button==5)) {
-            System.out.println("1. 저장    2. 삭제    3. 검색  4. 출력   5. 종료");
-            System.out.print("원하는 기능을 선택해주세요 : ");
-            button = Integer.parseInt(getInputString());
-            switch (button) {
-                case 1:
-                    arrayInfo[count] = arrayControl.addArrayInfo();
-                    break;
-                case 2:
-                    arrayControl.delArrayInfo(arrayInfo);
-                    break;
-                case 3:
-                    arrayControl.findArrayInfo(arrayInfo);
-                    break;
-                case 4 :
-                    arrayControl.showArrayInfo(arrayInfo);
-                default:
-                    break;
-            }
+            try {
+                showMenu();
+                button = Integer.parseInt(getInputString());
+                switch (button) {
+                    case 1:
+                        arrayInfo[count] = arrayControl.addArrayInfo();
+                        break;
+                    case 2:
+                        arrayControl.delArrayInfo(arrayInfo);
+                        break;
+                    case 3:
+                        arrayControl.findArrayInfo(arrayInfo);
+                        break;
+                    case 4:
+                        arrayControl.showArrayInfo(arrayInfo);
+                    default:
+                        break;
+                }
 
+            }catch (NumberFormatException e){
+                System.out.println("------------------------------------------------");
+                System.out.println("##### 숫자로 입력하여 주십시오 #####");
+            }
         }
 
+    }
+
+    private static void showMenu() {
+        System.out.println("------------------------------------------------");
+        System.out.println("1. 저장    2. 삭제    3. 검색    4. 출력    5. 종료");
+        System.out.println("------------------------------------------------");
+        System.out.print("원하는 기능을 선택해주세요 : ");
     }
 
     public static String getInputString(){
