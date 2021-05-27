9817b0be13d251058d8224ba7da9ea47645517f2
KimYongMin2 <kym960304@naver.com>
2021-05-27 15:13:22 +0900

    03번 문제
    위치변환

--- java_project/FirstJava/src/homework/ArrShuffle.java
+++ java_project/FirstJava/src/homework/ArrShuffle.java
@@ -3,31 +3,6 @@ package homework;
 import java.util.Scanner;
 
 public class ArrShuffle {
-    public static void main(String[] args) {
-        int processionRow;
-        int processionColumn;
-        System.out.print("행(가로)수를 입력해주세요 : ");
-        processionRow = getInputInt();
-        System.out.print("열(세로)수를 입력해주세요 : ");
-        processionColumn = getInputInt();
-        int[][] score = new int[processionRow][processionColumn];
-
-        for (int i = 0; i < score.length; i++) {
-            for (int j = 0; j < score[i].length; j++) {
-                System.out.print((i+1) + "번째의 " + (j+1) + "번 수를 입력하여 주세요 : ");
-                score[i][j] = getInputInt();
-            }
-        }
-
-        showInputScore(score);
-
-        ArrShuffle(score);
-
-        showInputScore(score);
-
-
-    }
-    
     private static void ArrShuffle(int[][] arr){
         int[] temp;
         for (int i = arr.length-1; i > 0; i--) {
@@ -51,5 +26,29 @@ public class ArrShuffle {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         return Integer.parseInt(input);
+    }
+
+    public static void main(String[] args) {
+        int processionRow;
+        int processionColumn;
+        System.out.print("행(가로)수를 입력해주세요 : ");
+        processionRow = getInputInt();
+        System.out.print("열(세로)수를 입력해주세요 : ");
+        processionColumn = getInputInt();
+        int[][] score = new int[processionRow][processionColumn];
+
+        for (int i = 0; i < score.length; i++) {
+            for (int j = 0; j < score[i].length; j++) {
+                System.out.print((i + 1) + "번째의 " + (j + 1) + "번 수를 입력하여 주세요 : ");
+                score[i][j] = getInputInt();
+            }
+        }
+
+        showInputScore(score);
+
+        System.out.println("순서를 바꿉니다.");
+        ArrShuffle(score);
+
+        showInputScore(score);
     }
 }
