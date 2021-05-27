17132275b31c539ce8b2ae80fda42f3d90a08227
KimYongMin2 <kym960304@naver.com>
2021-05-27 10:20:53 +0900

    1900년대 2000년대 구분

--- java_project/FirstJava/src/myProjectExtends/Person.java
+++ java_project/FirstJava/src/myProjectExtends/Person.java
@@ -7,12 +7,21 @@ public class Person {
     private String name;
     private String idNumber;
     private int age;
+    private char checkSince;
     public Person(String name, String idNumber){
         this.name = name;
         this.idNumber = idNumber;
     }
     public void hello(){
-        this.age = Calendar.getInstance().get(Calendar.YEAR) - Integer.parseInt(idNumber.substring(0,2))-1899;
+        checkSince = idNumber.charAt(6);
+        if (checkSince == '-'){
+            checkSince = idNumber.charAt(7);
+        }
+        if (checkSince=='1'||checkSince=='2'){
+            this.age = Calendar.getInstance().get(Calendar.YEAR) - Integer.parseInt(idNumber.substring(0,2))-1899;
+        }else if(checkSince=='3'||checkSince=='4'){
+            this.age = Calendar.getInstance().get(Calendar.YEAR) - Integer.parseInt(idNumber.substring(0,2))-1999;
+        }
         System.out.println("안녕하세요. 저는 " + this.name + "입니다. " + this.age + "살 입니다.");
     }
 }
--- java_project/FirstJava/src/myProjectExtends/PersonMain.java
+++ java_project/FirstJava/src/myProjectExtends/PersonMain.java
@@ -3,7 +3,7 @@ package myProjectExtends;
 public class PersonMain {
     public static void main(String[] args) {
         String idNumber = "960304";
-        Person person = new Person("사람" , "960304");
+        Person person = new Person("사람" , "9603041");
         Male male = new Male("김용민", "960304-1******");
         Female female = new Female("김용순", "9504012******");
 
