bd57a4642543b78e8d2514ea01a28be6bb9299d5
KimYongMin2 <kym960304@naver.com>
2021-05-27 12:43:11 +0900

    charAt을 사용하여 한글자씩 출력

--- java_project/FirstJava/src/test/Test.java
+++ java_project/FirstJava/src/test/Test.java
@@ -2,6 +2,10 @@ package test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("JAVA");
+        String src = "ABCDE";
+        for (int i = 0; i < src.length(); i++) {
+            System.out.println("src.charAt("+i+"):" + src.charAt(i) );
+
+        }
     }
 }
