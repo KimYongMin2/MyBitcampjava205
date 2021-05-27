175fef92f7aa0caf5ce7ee353af82794008d593f
KimYongMin2 <kym960304@naver.com>
2021-05-27 20:48:38 +0900

    프로젝트01

---
+++ java_project/FirstJava/src/project1/Contact.java
@@ -0,0 +1,81 @@
+package project1;
+
+public class Contact {
+    private String name;
+    private String number;
+    private String email;
+    private String address;
+    private int birth;
+    private String group;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public int getBirth() {
+        return birth;
+    }
+
+    public void setBirth(int birth) {
+        this.birth = birth;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public void setGroup(String group) {
+        this.group = group;
+    }
+
+    public Contact(String name, String number, String email, String address, int birth, String group) {
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        this.address = address;
+        this.birth = birth;
+        this.group = group;
+    }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", number='" + number + '\'' +
+                ", email='" + email + '\'' +
+                ", address='" + address + '\'' +
+                ", birth=" + birth +
+                ", group='" + group + '\'' +
+                '}';
+    }
+
+
+}
