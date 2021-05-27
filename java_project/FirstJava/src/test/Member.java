007ec50642c86e6c4a3efc8a8b3562aa1f76834c
KimYongMin2 <kym960304@naver.com>
2021-05-27 16:58:41 +0900

    상속 test

--- java_project/FirstJava/src/test/Member.java
+++ java_project/FirstJava/src/test/Member.java
@@ -1,5 +1,36 @@
 package test;
 
-public class Member {
+public class Member extends BBB{
+    void hello(){
+        System.out.println("CCC");
+    }
 
+    public static void main(String[] args) {
+        AAA aaa = new AAA();
+        BBB bbb = new BBB();
+        Member member = new Member();
+
+        aaa.hello();
+        bbb.hello();
+        member.hello();
+
+        AAA aab = new BBB();
+        AAA aam = new Member();
+
+        aab.hello();
+        aam.hello();
+    }
+
+}
+
+class AAA{
+    void hello(){
+        System.out.println("AAA");
+    }
+}
+
+class BBB extends AAA{
+    void hello(){
+        System.out.println("BBB");
+    }
 }
