0c956fd1144c5a83362985a4063268fbd63a7a1c
KimYongMin2 <kym960304@naver.com>
2021-05-27 14:32:41 +0900

    01번 문제
    사용자가 직접 행렬 길이 추가

--- java_project/FirstJava/src/homework/MiniMax.java
+++ java_project/FirstJava/src/homework/MiniMax.java
@@ -4,11 +4,15 @@ import java.util.Scanner;
 
 public class MiniMax {
     public static void main(String[] args) {
-        int[] score = new int[5];
+        int arrayNum;
+        System.out.print("배열의 갯수를 입력해주세요 : ");
+        arrayNum = getInputInt();
+
+        int[] score = new int[arrayNum];
 
         for (int i = 0; i < score.length; i++) {
             System.out.print((i+1) + "번째 수를 입력하여 주세요 : ");
-            score[i] = getInputScore();
+            score[i] = getInputInt();
         }
 
         showInputScore(score);
@@ -45,7 +49,7 @@ public class MiniMax {
         }
         return temp;
     }
-    public static int getInputScore(){
+    public static int getInputInt(){
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         return Integer.parseInt(input);
