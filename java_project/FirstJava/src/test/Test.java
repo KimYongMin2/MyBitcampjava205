d00db21fac573ca811f6d834556817d7eb39575a
KimYongMin2 <kym960304@naver.com>
2021-05-28 10:50:57 +0900

    test용 커밋

--- java_project/FirstJava/src/test/Test.java
+++ java_project/FirstJava/src/test/Test.java
@@ -2,6 +2,7 @@ package test;
 
 public class Test {
     public static void main(String[] args) {
+        System.out.println("hi");
         String src = "ABCDE";
         for (int i = 0; i < src.length(); i++) {
             System.out.println("src.charAt("+i+"):" + src.charAt(i) );
