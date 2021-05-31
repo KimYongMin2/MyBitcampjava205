5d2262e27244dedc76ef3ee4310b2b19e35ce5df
KimYongMin2 <kym960304@naver.com>
2021-05-31 19:07:54 +0900

    테스트용 커밋

--- java_project/FirstJava/src/test/Test.java
+++ java_project/FirstJava/src/test/Test.java
@@ -2,7 +2,8 @@ package test;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("ㅎㅇ");
-        System.out.println();
+
+        char a =  0x39;
+        System.out.println(a);
     }
 }
