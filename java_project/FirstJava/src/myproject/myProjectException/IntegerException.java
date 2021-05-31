16ec6e787c6d2a5685528b09985e6072e9b79ee2
KimYongMin2 <kym960304@naver.com>
2021-05-31 19:26:23 +0900

    int Exception
    태어난 년도를 nextInt로 입력받고 일어나는 예외 처리

---
+++ java_project/FirstJava/src/myproject/myProjectException/IntegerException.java
@@ -0,0 +1,17 @@
+package myproject.myProjectException;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class IntegerException extends Throwable {
+    public static void main(String[] args) {
+        try {
+            System.out.print("태어난 년도를 입력해주세요 : ");
+            Scanner scanner = new Scanner(System.in);
+            int num = scanner.nextInt();
+            System.out.println(num);
+        }catch (InputMismatchException e){
+            System.out.println("숫자로 입력해주세요");
+        }
+    }
+}
