ea6770dba992a5b20a21243404a13d71be812fbf
KimYongMin2 <kym960304@naver.com>
2021-06-01 14:52:52 +0900

    FootballPlayer TreeSet
    확인문 변경

--- java_project/FirstJava/src/myproject/ListSet/FPlayerTreeSet.java
+++ java_project/FirstJava/src/myproject/ListSet/FPlayerTreeSet.java
@@ -12,9 +12,13 @@ public class FPlayerTreeSet {
         treeSet.add(new FootballPlayer("홍길동", 3, "asd", 17));
         treeSet.add(new FootballPlayer("홍길순", 4, "asd", 15));
         treeSet.add(new FootballPlayer("홍길동", 1, "asd", 17));
+        treeSet.add(new FootballPlayer("홍길동", 1, "bas", 17));
+        treeSet.add(new FootballPlayer("김이박", 2, "bas", 17));
         treeSet.add(new FootballPlayer("김이박", 2, "asd", 17));
         treeSet.add(new FootballPlayer("박이김", 5, "asd", 17));
         treeSet.add(new FootballPlayer("이박김", 2, "asd", 17));
+        treeSet.add(new FootballPlayer("홍길순", 4, "bas", 15));
+        treeSet.add(new FootballPlayer("박이김", 5, "bas", 17));
 
         Iterator<FootballPlayer> iterator = treeSet.iterator();
 
