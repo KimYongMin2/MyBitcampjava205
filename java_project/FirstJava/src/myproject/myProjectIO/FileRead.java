ef1c534f30f70d9373fcdf5d003117897a97e74c
KimYongMin2 <kym960304@naver.com>
2021-06-02 18:59:01 +0900

    파일 읽기

--- java_project/FirstJava/src/myproject/myProjectIO/FileRead.java
+++ java_project/FirstJava/src/myproject/myProjectIO/FileRead.java
@@ -6,40 +6,48 @@ import java.io.*;
 
 public class FileRead {
     public void fileRead() throws IOException {
-        System.out.println("-----------------------------------------");
-        System.out.println("2. 파일 읽기");
-        System.out.println("-----------------------------------------");
+        showMyMenu();
         System.out.println("폴더를 찾습니다");
         System.out.print("찾을 폴더명을 입력하세요 : ");
         String findDirname = ScannerUtil.getInputString();
         File findDir = new File("C:\\Users\\bitcamp\\Documents\\myDir\\" + findDirname);
         if (!findDir.exists()) {
-            System.out.println("-----------------------------------------");
-            System.out.println("폴더가 존재하지 않습니다.");
-            return;
+            unexistence();
         } else {
             System.out.println("파일을 찾습니다");
             System.out.print("찾을 파일명을 입력하세요 : ");
             String findFilename = ScannerUtil.getInputString();
             File findFile = new File("C:\\Users\\bitcamp\\Documents\\myDir\\" + findDirname + "\\" + findFilename + ".txt");
             if (!findFile.exists()) {
-                System.out.println("-----------------------------------------");
-                System.out.println("파일이 존재하지 않습니다.");
+                unexistence();
             } else {
-                BufferedReader in = new BufferedReader(new FileReader(findFile));
-                String str = null;
-
-                System.out.println("-----------------------------------------");
-                while (true) {
-                    str = in.readLine();
-
-                    if (str == null) {
-                        break;
-                    }
-
-                    System.out.println(str);
-                }
+                showFileContents(findFile);
             }
         }
+    }
+
+    public void unexistence() {
+        System.out.println("-----------------------------------------");
+        System.out.println("존재하지 않습니다.");
+        return;
+    }
+
+    public void showFileContents(File findFile) throws IOException {
+        BufferedReader in = new BufferedReader(new FileReader(findFile));
+        String str = null;
+        System.out.println("-----------------------------------------");
+        while (true) {
+            str = in.readLine();
+            if (str == null) {
+                break;
+            }
+            System.out.println(str);
+        }
+    }
+
+    public void showMyMenu() {
+        System.out.println("-----------------------------------------");
+        System.out.println("2. 파일 읽기");
+        System.out.println("-----------------------------------------");
     }
 }
