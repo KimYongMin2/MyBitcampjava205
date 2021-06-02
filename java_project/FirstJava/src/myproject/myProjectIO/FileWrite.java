afdcb42c8626204269368a3f8e032cbe169951dc
KimYongMin2 <kym960304@naver.com>
2021-06-02 18:59:06 +0900

    파일 쓰기

--- java_project/FirstJava/src/myproject/myProjectIO/FileWrite.java
+++ java_project/FirstJava/src/myproject/myProjectIO/FileWrite.java
@@ -11,9 +11,8 @@ import java.util.Calendar;
 
 public class FileWrite {
     public void fileWrite() throws IOException {
-        System.out.println("-----------------------------------------");
-        System.out.println("1. 파일 저장");
-        System.out.println("-----------------------------------------");
+        showMyMenu();
+
         System.out.print("저장할 폴더 명을 입력해주세요 : ");
         String saveDir = ScannerUtil.getInputString();
         File newDir = new File("C:\\Users\\bitcamp\\Documents\\myDir\\" + saveDir);
@@ -21,23 +20,23 @@ public class FileWrite {
             newDir.mkdir();
         }
 
-        Calendar cal = Calendar.getInstance();
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        String now = simpleDateFormat.format(cal.getTime());
-
+        String now = calenderNowData();
         System.out.println("현재날짜 : " + now);
         System.out.println("---------------------------------------------------------");
         System.out.print("제목을 입력하여 주세요 : ");
         String title = ScannerUtil.getInputString();
         System.out.print("1줄 내용을 입력하여 주세요 : ");
         String contents = ScannerUtil.getInputString();
+        
+        saveFile(saveDir, now, title, contents);
 
-        System.out.println();
+    }
+
+    public void saveFile(String saveDir, String now, String title, String contents) throws IOException {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("메모를 작성합니다");
         BufferedWriter out = new BufferedWriter(new FileWriter("C:\\Users\\bitcamp\\Documents\\myDir\\"
                 + saveDir + "\\" + now + "_" + title + ".txt"));
-
-
-        System.out.println("메모를 작성합니다");
         out.write("현재시간 : " + now);
         out.newLine();
         out.write("제목 : " + title);
@@ -47,5 +46,18 @@ public class FileWrite {
         out.close();
         System.out.println("작성 완료");
         System.out.println();
+    }
+
+    public String calenderNowData() {
+        Calendar cal = Calendar.getInstance();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String now = simpleDateFormat.format(cal.getTime());
+        return now;
+    }
+
+    public void showMyMenu() {
+        System.out.println("-----------------------------------------");
+        System.out.println("1. 파일 저장");
+        System.out.println("-----------------------------------------");
     }
 }
