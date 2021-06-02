8b738eccce8811b41cf096c9d40b0f41bbff458d
KimYongMin2 <kym960304@naver.com>
2021-06-02 19:16:47 +0900

    FootballPlayerFile Main

--- java_project/FirstJava/src/myproject/myProjectIO/FootballPlayerFile.java
+++ java_project/FirstJava/src/myproject/myProjectIO/FootballPlayerFile.java
@@ -5,19 +5,21 @@ import java.util.ArrayList;
 
 public class FootballPlayerFile {
     public static void main(String[] args) throws IOException, ClassNotFoundException {
+        //저장할 경로 지정
         ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("C:\\Users\\bitcamp\\Documents\\myDir\\player\\player.txt"));
 
+        //리스트생성
         ArrayList<FootballPlayer> list = new ArrayList<>();
-        list.add(new FootballPlayer("홍길동",1,"한국", 20));
-        list.add(new FootballPlayer("홍길순",2,"한국", 21));
-        list.add(new FootballPlayer("김용민",3,"한국", 26));
-        list.add(new FootballPlayer("김용순",4,"한국", 27));
-        list.add(new FootballPlayer("김개똥",5,"한국", 24));
+        //리스트에 정보 저장
+        setListNewObject(list);
+        //경로에 리스트 저장
         out.writeObject(list);
+        //끝마침
         out.close();
 
         System.out.println("인스턴스 저장완료");
 
+        //읽을 경로 지정
         ObjectInputStream in = new ObjectInputStream(new FileInputStream("C:\\Users\\bitcamp\\Documents\\myDir\\player\\player.txt"));
 
         //복원순서는 저장순서에 맞게 복원
@@ -28,5 +30,13 @@ public class FootballPlayerFile {
         for (FootballPlayer footballPlayer : list1) {
             System.out.println(footballPlayer);
         }
+    }
+
+    private static void setListNewObject(ArrayList<FootballPlayer> list) {
+        list.add(new FootballPlayer("홍길동",1,"한국", 20));
+        list.add(new FootballPlayer("홍길순",2,"한국", 21));
+        list.add(new FootballPlayer("김용민",3,"한국", 26));
+        list.add(new FootballPlayer("김용순",4,"한국", 27));
+        list.add(new FootballPlayer("김개똥",5,"한국", 24));
     }
 }
