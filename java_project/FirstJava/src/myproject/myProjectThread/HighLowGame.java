384f21863fb0ec1757ca1b95774e587602bfcb23
KimYongMin2 <kym960304@naver.com>
2021-06-03 16:35:41 +0900

    01 게임 메인문

--- java_project/FirstJava/src/myproject/myProjectThread/HighLowGame.java
+++ java_project/FirstJava/src/myproject/myProjectThread/HighLowGame.java
@@ -3,7 +3,7 @@ package myproject.myProjectThread;
 public class HighLowGame {
     public static boolean check = false;
     public static void main(String[] args) {
-        
+
         int comAnswer = (int)(Math.random() * 100);
         System.out.println("랜덤 숫자가 생성되었습니다.");
         CompareAnswer compareAnswer = new CompareAnswer(comAnswer);
