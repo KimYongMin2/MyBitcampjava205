48a5148d6ffaae37a720054320a29b6cb7ca0864
KimYongMin2 <kym960304@naver.com>
2021-06-04 15:46:48 +0900

    소켓서버

--- java_project/FirstJava/src/socket/SocketServer.java
+++ java_project/FirstJava/src/socket/SocketServer.java
@@ -4,16 +4,40 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+import java.io.DataInputStream;
+
+
 public class SocketServer {
+
     public static void main(String[] args) {
 
         try {
-            ServerSocket socket = new ServerSocket(9999);
-            
-            Socket s = socket.accept(); //Socket에서 연결요청이 올 때까지 대기
+            ServerSocket serverSocket = new ServerSocket(9999);
+
+            System.out.println("서버 실행 !");
+
+            Socket socket = serverSocket.accept(); // Socket에서 연결 요청이 올때 까지 대기
+
+            if(socket != null) {
+                System.out.println("클라이언트의 접속 요청 : Socket 생성");
+            }
+
+            DataInputStream din = new DataInputStream(socket.getInputStream());
+
+            String msg = din.readUTF();
+            System.out.println(msg);
+
+            din.close();
+            socket.close();
+
+
+
         } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
 
         }
+
     }
+
 }
