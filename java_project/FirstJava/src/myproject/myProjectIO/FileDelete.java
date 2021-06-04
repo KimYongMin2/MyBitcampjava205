a10997e414ca8d7db6942194590facecb9a10980
KimYongMin2 <kym960304@naver.com>
2021-06-04 17:59:40 +0900

    file delete 간략

--- java_project/FirstJava/src/myproject/myProjectIO/FileDelete.java
+++ java_project/FirstJava/src/myproject/myProjectIO/FileDelete.java
@@ -6,26 +6,40 @@ import java.io.File;
 import java.io.IOException;
 
 public class FileDelete {
+    private String findDirname, findFilename;
+    private File findDir, findFile;
     public void saveFileDelete() throws IOException {
         showMyMenu();
-        System.out.println("파일을 삭제합니다");
-        System.out.print("삭제할 파일이 존재하는 폴더명을 입력하세요 : ");
-        String findDirname = ScannerUtil.getInputString();
-        File findDir = new File("C:\\Users\\bitcamp\\Documents\\myDir\\" + findDirname);
+        setFindDelDirName();
         if (!findDir.exists()) {
             unexistence();
         } else {
-            System.out.print("삭제할 파일명을 입력하세요 : ");
-            String findFilename = ScannerUtil.getInputString();
-            File findFile = new File("C:\\Users\\bitcamp\\Documents\\myDir\\" + findDirname + "\\" + findFilename + ".txt");
+            setDelFileName();
             if (!findFile.exists()) {
                 unexistence();
             } else {
-                findFile.delete();
-                System.out.println("-----------------------------------------");
-                System.out.println("삭제 완료");
+                fileDel();
             }
         }
+    }
+
+    private void fileDel() {
+        findFile.delete();
+        System.out.println("-----------------------------------------");
+        System.out.println("삭제 완료");
+    }
+
+    private void setDelFileName() {
+        System.out.print("삭제할 파일명을 입력하세요 : ");
+        findFilename = ScannerUtil.getInputString();
+        findFile = new File("C:\\Users\\bitcamp\\Documents\\myDir\\" + findDirname + "\\" + findFilename + ".txt");
+    }
+
+    private void setFindDelDirName() {
+        System.out.println("파일을 삭제합니다");
+        System.out.print("삭제할 파일이 존재하는 폴더명을 입력하세요 : ");
+        findDirname = ScannerUtil.getInputString();
+        findDir = new File("C:\\Users\\bitcamp\\Documents\\myDir\\" + findDirname);
     }
 
     public void unexistence() {
