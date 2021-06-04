3af4186e200633118b81a71e8345aa5e9a5b50d3
KimYongMin2 <kym960304@naver.com>
2021-06-04 16:43:50 +0900

    메세지 서버

--- java_project/FirstJava/src/socket/MessageServer.java
+++ java_project/FirstJava/src/socket/MessageServer.java
@@ -1,84 +1,62 @@
 package socket;
 
 import common.util.CloseUtil;
-import common.util.ScannerUtil;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class MessageServer {
+
     public static void main(String[] args) {
 
-        ServerSocket serverSocket = null;
-        Socket socket = null;
+        ServerSocket ss = null;
+        Socket s = null;
 
-        DataInputStream dataInputStream = null;
-        DataOutputStream dataOutputStream = null;
+        DataInputStream din = null;
+        DataOutputStream dout = null;
 
         try {
-            serverSocket = new ServerSocket(8888);
 
-            socket = serverSocket.accept();
+            ss = new ServerSocket(8888);
+            System.out.println("서버 실행");
+            System.out.println("----------------------------");
+            s = ss.accept();
+            System.out.println("client 접속");
+            System.out.println("----------------------------");
 
-            dataInputStream = new DataInputStream(socket.getInputStream());
-            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            din = new DataInputStream(s.getInputStream());
+            dout = new DataOutputStream(s.getOutputStream());
 
-            String str1 = null; //받는
-            String str2 = " "; //보내는
+            Scanner sc = new Scanner(System.in);
+
+            String str1 = null; // 받는 메시지
+            String str2 = ""; // 보내는 메시지
 
             while (!str2.equals("exit")) {
-                str1 = dataInputStream.readUTF();
-                System.out.println("Client Message" + str1);
 
-                str2 = ScannerUtil.getInputString();
-                dataOutputStream.writeUTF(str2);
-                dataOutputStream.flush();
+                str1 = din.readUTF();
+                System.out.println("Client Message : " + str1);
+
+                str2 = sc.nextLine();
+                dout.writeUTF(str2);
             }
-
-
-
 
         } catch (IOException e) {
             e.printStackTrace();
-        } finally {
-            CloseUtil.close(serverSocket);
-            CloseUtil.close(socket);
-            CloseUtil.close(dataInputStream);
-            CloseUtil.close(dataOutputStream);
-//            if (dataInputStream != null){
-//                try {
-//                    dataInputStream.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if (dataOutputStream != null){
-//                try {
-//                    dataOutputStream.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if (socket != null){
-//                try {
-//                    socket.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
-//            if (serverSocket != null){
-//                try {
-//                    serverSocket.close();
-//                } catch (IOException e) {
-//                    e.printStackTrace();
-//                }
-//            }
 
+        } finally {
+
+            CloseUtil.close(din);
+            CloseUtil.close(dout);
+            CloseUtil.close(s);
+            CloseUtil.close(ss);
 
         }
 
     }
+
 }
