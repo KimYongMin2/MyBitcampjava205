48abcebe84b0e4c8757cef7f16c50b362898c792
KimYongMin2 <kym960304@naver.com>
2021-06-04 15:46:41 +0900

    클라이언트 서버접속

--- java_project/FirstJava/src/socket/ClientSocket.java
+++ java_project/FirstJava/src/socket/ClientSocket.java
@@ -5,15 +5,28 @@ import java.io.IOException;
 import java.net.Socket;
 
 public class ClientSocket {
+
     public static void main(String[] args) {
-        //SocketServer Ip, Port connection : Socket 생성
+
+        // SocketServer의 IP와 Port 번호로 연결신청 : Socket 객체를 생성
         try {
-            Socket  socket = new Socket("localhost", 9999);
-            if(socket != null){
-                System.out.println("서버접속 완료");
+            Socket socket = new Socket("localhost", 9999);
+
+            if (socket != null) {
+                System.out.println("서버 접속 완료");
             }
+
+            DataOutputStream dout = new DataOutputStream(socket.getOutputStream());
+
+            dout.writeUTF("Hi~!!! Client Message");
+            dout.flush();
+            dout.close();
+            socket.close();
+
         } catch (IOException e) {
+            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
+
 }
