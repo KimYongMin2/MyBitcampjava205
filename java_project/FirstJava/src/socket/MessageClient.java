82e69c39015901f053f1545a77ac9e1e5541e50e
KimYongMin2 <kym960304@naver.com>
2021-06-04 16:08:12 +0900

    message client

---
+++ java_project/FirstJava/src/socket/MessageClient.java
@@ -0,0 +1,43 @@
+package socket;
+
+import common.util.CloseUtil;
+import common.util.ScannerUtil;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class MessageClient {
+    public static void main(String[] args) {
+        Socket socket = null;
+
+        DataInputStream dataInputStream = null;
+        DataOutputStream dataOutputStream = null;
+        try {
+            socket = new Socket("localhost", 8888);
+
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            String str1 = null; //받기
+            String str2 = " "; //보내기
+
+            while (!str2.equals("exit")){
+                str1 = dataInputStream.readUTF();
+                System.out.println("Server Message : " + str1);
+
+                str2 = ScannerUtil.getInputString();
+                dataOutputStream.writeUTF(str2);
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            CloseUtil.close(socket);
+            CloseUtil.close(dataInputStream);
+            CloseUtil.close(dataOutputStream);
+        }
+    }
+}
