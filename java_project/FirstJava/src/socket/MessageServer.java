62e13c3ebc5e45c9949dd2a1c4a0c9520f6d0e0a
KimYongMin2 <kym960304@naver.com>
2021-06-04 15:27:25 +0900

    소켓채팅메세지서버

---
+++ java_project/FirstJava/src/socket/MessageServer.java
@@ -0,0 +1,80 @@
+package socket;
+
+import common.util.ScannerUtil;
+import javafx.scene.transform.Scale;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class MessageServer {
+    public static void main(String[] args) {
+
+        ServerSocket serverSocket = null;
+        Socket socket = null;
+
+        DataInputStream dataInputStream = null;
+        DataOutputStream dataOutputStream = null;
+
+        try {
+            serverSocket = new ServerSocket(8888);
+
+            socket = serverSocket.accept();
+
+            dataInputStream = new DataInputStream(socket.getInputStream());
+            dataOutputStream = new DataOutputStream(socket.getOutputStream());
+
+            String str1 = null; //받는
+            String str2 = null; //보내는
+
+            while (!str2.equals("exit")) {
+                str1 = dataInputStream.readUTF();
+                System.out.println(str1);
+
+                str2 = ScannerUtil.getInputString();
+                dataOutputStream.writeUTF(str2);
+                dataOutputStream.flush();
+            }
+
+
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (dataInputStream != null){
+                try {
+                    dataInputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (dataOutputStream != null){
+                try {
+                    dataOutputStream.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (socket != null){
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (serverSocket != null){
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+        }
+
+    }
+}
