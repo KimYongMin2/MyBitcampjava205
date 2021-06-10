ab495226831096af9a86c134a493078dc8cf420f
KimYongMin2 <kym960304@naver.com>
2021-06-10 17:47:43 +0900

    스캐너 변경

--- java_project/FirstJava/src/project3/ScannerUtil.java
+++ java_project/FirstJava/src/project3/ScannerUtil.java
@@ -3,18 +3,19 @@ package project3;
 import java.util.Scanner;
 
 public class ScannerUtil {
+    static Scanner scanner;
     public static String getInputString(){
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
 
     public static int getInputInteger(){
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         return Integer.parseInt(scanner.nextLine());
     }
 
     public static char getInputChar(){
-        Scanner scanner = new Scanner(System.in);
+        scanner = new Scanner(System.in);
         return scanner.nextLine().charAt(0);
     }
 }
