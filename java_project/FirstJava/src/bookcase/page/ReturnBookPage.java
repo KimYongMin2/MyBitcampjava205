e0c13ab0396fd9a7dd9e28c7a02f3832581749c8
KimYongMin2 <kym960304@naver.com>
2021-06-28 09:35:17 +0900

    test

---
+++ java_project/FirstJava/src/bookcase/page/ReturnBookPage.java
@@ -0,0 +1,42 @@
+package bookcase.page;
+
+import bookcase.ReturnBook;
+import bookcase.ShowBookList;
+import bookcase.object.Member;
+import bookcase.show.Show;
+import bookcase.util.ScannerUtil;
+
+public class ReturnBookPage extends ShowBookList implements Show {
+	
+	private Member member;
+	private int menuButton = 0;
+	
+	public ReturnBookPage(Member member) {
+		this.member = member;
+	}
+
+	public void BookReturnStart() {
+		ReturnBook returnbooks = new ReturnBook(member);
+		while (menuButton != 2) {
+			try {
+				showBookReturnMenu();
+				menuButton = ScannerUtil.getInputIntegerS(">> 원하시는 메뉴를 선택해주세요 : ");
+				switch (menuButton) {
+				case 1: // 도서 반납
+					showMyUsingBook(member);
+					returnbooks.returnBook();
+					break;
+				case 2: // 도서 종료
+					System.out.println("[!] 이전 단계로 돌아갑니다.");
+					System.out.println();
+					break;
+				default: // 이전 단계
+					System.out.println("error : 잘못된 입력입니다.");
+					break;
+				}
+			} catch (NumberFormatException e) {
+				System.out.println("error : 숫자로 입력해주세요.");
+			}
+		}
+	}
+}
---
+++ java_project/FirstJava/src/bookcase/util/CloseUtil.java
@@ -0,0 +1,38 @@
+package bookcase.util;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class CloseUtil {
+    public static void close(Statement statement){
+        if (statement != null){
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void close(ResultSet resultSet){
+        if (resultSet != null){
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void close(PreparedStatement preparedStatement){
+        if (preparedStatement != null){
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
